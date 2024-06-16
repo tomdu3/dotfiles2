4b23f5364b0d5fc29b987f9c0b640249c0c68a03
Tomislav Dukez <tomdu3@ymail.com>
2024-06-16 23:54:33 +0200

    add lazyvim plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -9,8 +9,16 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   spec = {
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-
+    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
+      colorscheme = "sonokai",
+    } },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.lang.typescript" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "plugins" },
   },
   defaults = {
